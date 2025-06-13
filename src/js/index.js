45f1fecfeaffc8be33a27ce87ee56de3d2962437
Romaios <romanios108@gmail.com>
2025-06-13 21:11:43 +0400

    add advantage section and mobile section

--- src/js/index.js
+++ src/js/index.js
@@ -15,3 +15,11 @@ counter();
 // Кнопка наверх
 import buttonTop  from './modules/button-top';
 buttonTop();
+
+// Секция example-mobile
+import exampleMobile from './modules/example-mobile';
+exampleMobile();
+
+// Dropdown Advantages
+import advantadesAccordion from './modules/advantages-accordion';
+advantadesAccordion();
